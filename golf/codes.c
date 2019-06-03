0f0f4685b0c32a846b6ea9f2ece7dba3b8fff76b
zhiayang <zhiayang@gmail.com>
2019-06-03 22:43:20 +0800

    add emulator

--- golf/codes.c
+++ golf/codes.c
@@ -5,6 +5,10 @@
 // count2:
 // i;main(){for(;i++^9999;printf("%d,",i));}
 
+
 // count4:
 // main(i){printf("%d,",i++);if(i<1E4)main(i);}
 */
+
+__ma________d__m_________a___d__
+/*ma*/#include"my_code.c"/*asd*/
--- lscvm/lscvm-deasciiinator.cpp
+++ lscvm/lscvm-deasciiinator.cpp
@@ -7,10 +7,40 @@
 #include <string>
 #include <iostream>
 
+std::map<char, std::string> lookup;
+
+std::string createNumber(int num)
+{
+	if(num < 10)
+	{
+		return std::string(1, (char) (num + 'a'));
+	}
+	else if(num == 10)
+	{
+		return "cfM";
+	}
+	else if((num >= 'a' && num <= 'z') || num == '-'
+		|| num == '_' || num == '!')
+	{
+		return lookup[num];
+	}
+	else
+	{
+		std::string ret;
+
+		int x = num / 10;
+		ret = createNumber(x) + createNumber(10) + "M";
+
+		int y = num % 10;
+		ret += createNumber(y) + "A";
+
+		return ret;
+	}
+}
+
+
 int main()
 {
-	std::map<char, std::string> lookup;
-
 	lookup['a'] = "jjMjAhA";
 	lookup['b'] = "jjMjAiA";
 	lookup['c'] = "jjMjAjA";
@@ -46,11 +76,15 @@ int main()
 
 	while(true)
 	{
+		printf("string: ");
 		std::getline(std::cin, input);
 
 		std::string output;
-		for(size_t i = input.size(); i-- > 0; )
-			output += lookup[input[i]];
+
+		// the reason we consolidate all the 'P's at the end is so we can use this to set strings up on the stack,
+		// without printing -- we just don't copy the 'P' part.
+		for(size_t i = input.size(); i-- > 0;)
+			output += createNumber(input[i]);
 
 		for(size_t i = 0; i < input.size(); i++)
 			output += "P";
@@ -58,7 +92,6 @@ int main()
 		printf("\n%s\n", output.c_str());
 	}
 }
-
 
 
 
